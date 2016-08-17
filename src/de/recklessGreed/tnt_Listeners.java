f5f360bfbab2d5f1480c0d5d3368893163a80b85
recklessGreed148 <recklessGreed148@gmail.com>
2016-08-17 19:31:28 +0200

    Semi Finished. Need some Thinking and some Internet with Google to continue so no progress in train

--- src/de/recklessGreed/tnt_Listeners.java
+++ src/de/recklessGreed/tnt_Listeners.java
@@ -1,23 +1,28 @@
 package de.recklessGreed;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.*;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.PlayerLeashEntityEvent;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by recklessGreed on 08.08.16.
  */
+
 public class tnt_Listeners implements Listener
 {
     tnt_Listeners instance;
@@ -33,31 +38,81 @@ public class tnt_Listeners implements Listener
     {
         Player player = event.getPlayer();
 
-
-        Bukkit.getScheduler().scheduleSyncDelayedTask(mainClass, new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                player.openInventory(createFieldInventory(player));
-            }
-        },40L);
+        Bukkit.getScheduler().scheduleSyncDelayedTask(mainClass,
+                () -> player.openInventory(createFieldInventory(player)),40L);
 
     }
 
     @EventHandler
     public void onLeave(PlayerQuitEvent event)
     {
+        Player player = event.getPlayer();
 
+        if(mainClass.playerWorldMap.containsKey(player))
+        {
+            player.kickPlayer(ChatColor.AQUA + "Danke das du MineSweeper gespielt hast!");
+            World world = mainClass.playerWorldMap.get(player);
+            Bukkit.unloadWorld(world, false);
+            //delete World!!!!
+            //google !
+        }
+        else
+        {
+            // Do Stuff
+        }
     }
+
+    /*
+        Finished Join/Leave Section
+     */
 
     @EventHandler
     public void onInventoryClose(InventoryCloseEvent event)
     {
         Inventory inv = event.getInventory();
+        Player player = (Player) event.getPlayer();
+
         if(inv.getName().equals("Feldgröße"))
         {
-            
+            int width = inv.getItem(13).getAmount();
+            int height = inv.getItem(31).getAmount();
+            int numberOfFields = width*height;
+            int tntCount = inv.getItem(49).getAmount();
+            double percentage = tntCount / numberOfFields;
+
+            //create New World
+            File srcFolder = new File("./template");
+            File destFolder = new File("./gamemap_"+player.getName());
+            try
+            {
+                mainClass.copyFolder(srcFolder, destFolder);
+                World gameWorld = Bukkit.createWorld(new WorldCreator("gamemap_"+player.getName()));
+                //create Field with correct Size
+                Location spawn = gameWorld.getSpawnLocation();
+
+                for(int w = 0; w < width; w++)
+                {
+                    for(int h = 0; h < height; h++)
+                    {
+                        Location currentGame = new Location(gameWorld, spawn.getX()+ w, spawn.getY()-1, spawn.getZ()+ h);
+                        Location currentCover = new Location(gameWorld, spawn.getX()+ w, spawn.getY(), spawn.getZ()+ h);
+
+                        //cover up
+                        currentCover.getBlock().setType(Material.SAND);
+                    }
+                }
+
+                //createMines(); // The question is how!
+
+                //give Player the correct Items
+                player.teleport(spawn);
+            } catch (IOException e)
+            {
+                e.printStackTrace();
+                System.out.println(ChatColor.RED + "Either Source or Destination not accesable!");
+            }
+
+
         }
     }
 
@@ -85,54 +140,54 @@ public class tnt_Listeners implements Listener
                 }
 
             }
-            else if (clickedItem.getType() == Material.REDSTONE_BLOCK)
+            else if (clickedItem.getType() == Material.REDSTONE_BLOCK) //decrease
             {
                 ItemStack copyOf;
-                if (event.getSlot() / 9 == 1)
+                if (event.getSlot() / 9 == 1) //width
                 {
-                    copyOf = event.getCurrentItem().clone();
+                    copyOf = inv.getItem(13).clone();
                     if(copyOf.getAmount() >= 6)
                         copyOf.setAmount(copyOf.getAmount()-1);
-                    inv.setItem(event.getSlot(), copyOf);
+                    inv.setItem(13, copyOf);
                 }
-                else if (event.getSlot() / 9 == 3)
+                else if (event.getSlot() / 9 == 3) //height
                 {
-                    copyOf = event.getCurrentItem().clone();
+                    copyOf = inv.getItem(31).clone();
                     if(copyOf.getAmount() >= 6)
                         copyOf.setAmount(copyOf.getAmount()-1);
-                    inv.setItem(event.getSlot(), copyOf);
+                    inv.setItem(31, copyOf);
                 }
-                else if (event.getSlot() / 9 == 5)
+                else if (event.getSlot() / 9 == 5) //tntCount
                 {
-                    copyOf = event.getCurrentItem().clone();
+                    copyOf = inv.getItem(49).clone();
                     if(copyOf.getAmount() >= (inv.getItem(13).getAmount() * inv.getItem(31).getAmount())/10)
                         copyOf.setAmount(copyOf.getAmount()-1);
-                    inv.setItem(event.getSlot(), copyOf);
+                    inv.setItem(49, copyOf);
                 }
             }
-            else if (clickedItem.getType() == Material.EMERALD_BLOCK)
+            else if (clickedItem.getType() == Material.EMERALD_BLOCK) //increase
             {
                 ItemStack copyOf;
-                if (event.getSlot() / 9 == 1)
+                if (event.getSlot() / 9 == 1) //width
                 {
-                    copyOf = event.getCurrentItem().clone();
+                    copyOf = inv.getItem(13).clone();
                     if(copyOf.getAmount() <= 11)
                         copyOf.setAmount(copyOf.getAmount()+1);
-                    inv.setItem(event.getSlot(), copyOf);
+                    inv.setItem(13, copyOf);
                 }
-                else if (event.getSlot() / 9 == 3)
+                else if (event.getSlot() / 9 == 3) //height
                 {
-                    copyOf = event.getCurrentItem().clone();
+                    copyOf = inv.getItem(31).clone();
                     if(copyOf.getAmount() <= 11)
                         copyOf.setAmount(copyOf.getAmount()+1);
-                    inv.setItem(event.getSlot(), copyOf);
+                    inv.setItem(31, copyOf);
                 }
-                else if (event.getSlot() / 9 == 5)
+                else if (event.getSlot() / 9 == 5) //tntCount
                 {
-                    copyOf = event.getCurrentItem().clone();
+                    copyOf = inv.getItem(49).clone();
                     if(copyOf.getAmount() < (inv.getItem(13).getAmount() * inv.getItem(31).getAmount())*0.9)
                         copyOf.setAmount(copyOf.getAmount()+1);
-                    inv.setItem(event.getSlot(), copyOf);
+                    inv.setItem(49, copyOf);
                 }
             }
             event.setCancelled(true);
@@ -171,6 +226,68 @@ public class tnt_Listeners implements Listener
         return creator;
     }
 
+    /*
+        Finished Inventory Section
+     */
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent event)
+    {
+        Player player = event.getPlayer();
+        Block block = event.getBlock();
+
+        if(block.getType().equals(Material.SAND))
+        {
+            Location checkAt = block.getLocation().add(0,-1,0);
+            Block gameBlock = checkAt.getBlock();
+
+            if(gameBlock.getType().equals(Material.TNT))
+            {
+                //Game Ends
+                //replace every Block with tnt or RedstoneBlock
+
+                Location playerLoc = player.getLocation();
+                for(int i = 1; i < 5; i++)
+                {
+                    playerLoc.add(i*1 ,0,i*1).getBlock().setType(Material.TNT);
+                    playerLoc.add(-i*1,0,i*1).getBlock().setType(Material.TNT);
+                    playerLoc.add(i*1 ,0,-i*1).getBlock().setType(Material.TNT);
+                    playerLoc.add(-i*1,0,-i*1).getBlock().setType(Material.TNT);
+                    playerLoc.add(i*1 ,-1,i*1).getBlock().setType(Material.REDSTONE_BLOCK);
+                    playerLoc.add(-i*1,-1,i*1).getBlock().setType(Material.REDSTONE_BLOCK);
+                    playerLoc.add(i*1 ,-1,-i*1).getBlock().setType(Material.REDSTONE_BLOCK);
+                    playerLoc.add(-i*1,-1,-i*1).getBlock().setType(Material.REDSTONE_BLOCK);
+                }
+
+                Bukkit.getScheduler().scheduleSyncDelayedTask(mainClass, () -> player.kickPlayer(
+                        ChatColor.AQUA + "Danke das du Minesweeper gespielt hast."), 200L);
+            }
+            else
+            {
+                //checkSurroundings
+            }
+        }
+    }
+
+    /*
+        Finished Block Section
+     */
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageEvent event)
+    {
+        Entity entity = event.getEntity();
+        if(entity instanceof Player)
+        {
+            Player player = (Player) entity;
+            //if(player.hasMetadata("tnt_ingame"))
+            event.setCancelled(true);
+        }
+    }
+
+    /*
+        Finished Damage Section
+     */
 
     public tnt_Listeners getInstance()
     {
--- src/de/recklessGreed/tnt_Main.java
+++ src/de/recklessGreed/tnt_Main.java
@@ -1,10 +1,15 @@
 package de.recklessGreed;
 
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.util.HashMap;
 
 /**
  * Created by recklessGreed on 08.08.16.
@@ -13,6 +18,8 @@ public class tnt_Main extends JavaPlugin
 {
     Listener tnt_events;
     tnt_Main instance;
+    HashMap<Player, World> playerWorldMap = new HashMap<Player, World>();
+
 
 
     @Override
@@ -44,5 +51,51 @@ public class tnt_Main extends JavaPlugin
     public tnt_Main getInstance()
     {
         return instance;
+    }
+
+    public static void copyFolder(File src, File dest) throws IOException
+    {
+
+        if (src.isDirectory())
+        {
+
+            //if directory not exists, create it
+            if (!dest.exists())
+            {
+                dest.mkdir();
+            }
+
+            //list all the directory contents
+            String files[] = src.list();
+
+            for (String file : files)
+            {
+                //construct the src and dest file structure
+                File srcFile = new File(src, file);
+                File destFile = new File(dest, file);
+                //recursive copy
+                copyFolder(srcFile, destFile);
+            }
+
+        }
+        else
+        {
+            //if file, then copy it
+            //Use bytes stream to support all file types
+            InputStream in = new FileInputStream(src);
+            OutputStream out = new FileOutputStream(dest);
+
+            byte[] buffer = new byte[1024];
+
+            int length;
+            //copy the file content in bytes
+            while ((length = in.read(buffer)) > 0)
+            {
+                out.write(buffer, 0, length);
+            }
+
+            in.close();
+            out.close();
+        }
     }
 }
